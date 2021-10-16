a35740607f48615e3d6bd6ab0152d8916236dc57
marianfoo <marianbsp@gmail.com>
2021-10-16 19:21:08 +0200

    feat: first version of error handler with odata request

--- src/controller/MainView.controller.js
+++ src/controller/MainView.controller.js
@@ -1,5 +1,10 @@
 sap.ui.define(["sap/ui/core/mvc/Controller"], function(Controller) {
   "use strict";
 
-  return Controller.extend("ui5.sandbox.controller.MainView", {});
+  return Controller.extend("ui5.sandbox.controller.MainView", {
+
+    dataRequest: function(event, number){
+      this.getOwnerComponent().getModel().read(`/Products(${number})`)
+    }
+  });
 });
