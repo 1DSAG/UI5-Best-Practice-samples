0e8c9594dcd7dd3cf34602533d76116ef8c6a26d
Marian Zeis <marianbsp@gmail.com>
2021-10-16 19:25:52 +0200

    feat(error-handling): add example from doc (#6)
    * doc(readme): fix link to codesandbox
    
    * chore: basic setup
    
    * feat: add proxy
    
    * feat: add northwind service to manifest
    
    * doc(readme): fix link to codesandbox
    
    * feat: first version of error handler with odata request

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const express = require("express");
+const express = require('express'),
+    cors_proxy = require('cors-anywhere');
 const app = express();
 const port = 3000;
 
 app.use(express.static("src"));
 
 app.listen(port, () => console.log(`UI5 sandbox listening on port ${port}!`));
+
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port2 = process.env.PORT || 8080;
+
+
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    //requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port2, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port2);
+});
+
+
